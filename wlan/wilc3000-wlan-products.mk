b2d8573669291657c2bfd94e955a340eb0eddbfd
Kris Bahnsen <kris@embeddedTS.com>
2022-01-06 11:36:02 -0800

    Mass rename of names and URLs to embeddedTS

--- wlan/wilc3000-wlan-products.mk
+++ wlan/wilc3000-wlan-products.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2017 Technologic Systems
+# Copyright (C) 2017-2022 Technologic Systems, Inc. dba embeddedTS
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
